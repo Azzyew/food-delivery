d01e21240f70917ce44e12f7b6cbf0c281007d91
Azzyew <laisadnevescosta@gmail.com>
2024-02-08 18:31:27 -0300

    aula 2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,38 @@
-import { View, FlatList } from 'react-native';
-import { CategoryButton } from '@/components/category-button';
+import { useState, useRef } from 'react';
+import { View, FlatList, SectionList, Text } from 'react-native';
+import { Link } from 'expo-router';
+import { CategoryButton } from '@/components/categoryButton';
 import { Header } from '@/components/header';
-import { CATEGORIES } from '@/utils/data/products';
-import { useState } from 'react';
+import { CATEGORIES, MENU } from '@/utils/data/products';
+import { Product } from '@/components/product';
+import { useCartStore } from '@/stores/cartStore';
 
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState(CATEGORIES[0]);
+  const cartStore = useCartStore();
+
+  const sectionListRef = useRef<SectionList>(null);
+
+  const cartQuantityItems = cartStore.products.reduce((total, product) => total +
+    product.quantity, 0);
+
+  const handleCategorySelect = (selectedCategory: string) => {
+    setSelectedCategory(selectedCategory);
+
+    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory);
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      })
+    }
+  }
 
   return (
     <View className="pt-12">
-      <Header title="Cardápio" cartQuantityItems={1} />
+      <Header title="Cardápio" cartQuantityItems={cartQuantityItems} />
 
       <FlatList 
         data={CATEGORIES}
@@ -17,14 +40,34 @@ export default function Home() {
         renderItem={({ item }) => (
           <CategoryButton
             title={item}
-            onPress={() => setSelectedCategory(item)}
+            onPress={() => handleCategorySelect(item)}
             isSelected={item === selectedCategory}
           />
         )}
         horizontal
-        className='max-h-10 mt-5'
+        className="h-14 mt-5"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
         showsHorizontalScrollIndicator={false}
+      />
+
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild >
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title }}) => 
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        }
+        className="p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 200 }}
       />
     </View>
   );
--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,6 +1,6 @@
 import { ActivityIndicator, View } from "react-native";
 
-export function Loading() {
+export const Loading = () => {
   return (
     <View className="flex-1 items-center justify-center bg-slate-900">
       <ActivityIndicator color="white" />
